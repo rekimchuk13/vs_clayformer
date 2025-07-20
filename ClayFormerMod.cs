1e12c66e468897a8b340b6093350c79d9efc6083
rekimchuk13 <105552163+rekimchuk13@users.noreply.github.com>
2025-07-20 03:45:48 +0300

    Update ClayFormerMod.cs

--- ClayFormerMod.cs
+++ ClayFormerMod.cs
@@ -1,4 +1,5 @@
-using System; 
+using System;
+using System.Collections.Generic;
 using HarmonyLib;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
@@ -11,6 +12,7 @@ namespace ClayFormer
     {
         private Harmony harmony;
         private static ICoreClientAPI staticCapi;
+        private static Dictionary<BlockPos, ClaymationEngine> activeEngines = new Dictionary<BlockPos, ClaymationEngine>();
 
         public override void StartClientSide(ICoreClientAPI api)
         {
@@ -18,10 +20,26 @@ namespace ClayFormer
             harmony = new Harmony("com.rekimchuk13.clayformer");
             harmony.PatchAll();
             api.Logger.Notification("[ClayFormer] Mod loaded with Harmony patches!");
+            api.Event.LeftWorld += OnLeftWorld;
+        }
+
+        private void OnLeftWorld()
+        {
+            foreach (var engine in activeEngines.Values)
+            {
+                engine?.Stop();
+            }
+            activeEngines.Clear();
         }
 
         public override void Dispose()
         {
+            foreach (var engine in activeEngines.Values)
+            {
+                engine?.Stop();
+            }
+            activeEngines.Clear();
+
             harmony?.UnpatchAll("com.rekimchuk13.clayformer");
             staticCapi = null;
             base.Dispose();
@@ -31,32 +49,63 @@ namespace ClayFormer
         {
             return staticCapi;
         }
+
+        public static void RegisterEngine(BlockPos pos, ClaymationEngine engine)
+        {
+            if (activeEngines.ContainsKey(pos))
+            {
+                activeEngines[pos]?.Stop();
+            }
+            activeEngines[pos] = engine;
+        }
+
+        public static void UnregisterEngine(BlockPos pos) 
+        {
+            if (activeEngines.ContainsKey(pos))
+            {
+                activeEngines[pos]?.Stop();
+                activeEngines.Remove(pos);
+            }
+        }
     }
 
     [HarmonyPatch(typeof(GuiDialogBlockEntityRecipeSelector), MethodType.Constructor)]
     [HarmonyPatch(new Type[] { typeof(string), typeof(ItemStack[]), typeof(Action<int>), typeof(Action), typeof(BlockPos), typeof(ICoreClientAPI) })]
     public class RecipeGuiPatch
     {
-        private static ClaymationEngine currentEngine;
-
         static void Prefix(BlockPos blockEntityPos, ICoreClientAPI capi, ref Action<int> onSelectedRecipe)
         {
             Action<int> originalOnSelect = onSelectedRecipe;
 
             Action<int> wrappedOnSelect = (selectedIndex) =>
             {
+                originalOnSelect?.Invoke(selectedIndex);
+                if (capi?.World?.BlockAccessor == null) return;
 
-                originalOnSelect(selectedIndex);
-
-                if (capi.World.BlockAccessor.GetBlockEntity(blockEntityPos) is BlockEntityClayForm be)
+                var blockEntity = capi.World.BlockAccessor.GetBlockEntity(blockEntityPos);
+                if (blockEntity is BlockEntityClayForm clayForm)
                 {
-                    currentEngine?.Stop();
-                    currentEngine = new ClaymationEngine(capi, be);
-                    currentEngine.Start();
+                    ClayFormerMod.UnregisterEngine(blockEntityPos);
+
+                    var newEngine = new ClaymationEngine(capi, clayForm);
+                    ClayFormerMod.RegisterEngine(blockEntityPos, newEngine);
+                    newEngine.Start();
                 }
             };
 
             onSelectedRecipe = wrappedOnSelect;
+        }
+    }
+
+    [HarmonyPatch(typeof(BlockEntityClayForm), "OnBlockRemoved")]
+    public class ClayFormRemovedPatch
+    {
+        static void Prefix(BlockEntityClayForm __instance)
+        {
+            if (__instance?.Pos != null)
+            {
+                ClayFormerMod.UnregisterEngine(__instance.Pos);
+            }
         }
     }
 }
